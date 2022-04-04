197986592e781502cadd471fb2dcc93b374cf4b4
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2022-04-04 20:37:18 +0700

    Add files via upload

--- Tubes IF1210 Daspro/variables.py
+++ Tubes IF1210 Daspro/variables.py
@@ -6,48 +6,80 @@
 # username, password : string
 # data, users : csv
 
+# SUBPROGRAM UTAMA - CSV PARSER
+# 1. Membaca csv dan mengubahnya jadi string
+def bacacsv(namafile):
+    data = open(namafile)
+    stringdata = data.read()
+    data.close()
+    # menambahkan "\n" jika belum
+    if not(stringdata.endswith("\n")):
+        stringdata += "\n"
+    return stringdata
+
+# 2. Banyak character dalam csv
+def banyakchar(namafile):
+    stringdata = bacacsv(namafile)
+    # banyak char pada data
+    chara = 0
+    for row in stringdata:
+        chara += 1
+    return chara
+
+# 3. Banyak kolom dalam csv
+def banyakkolom(namafile):
+    stringdata = bacacsv(namafile)
+    # hitung jumlah baris dan kolom dengan for loop
+    kolom = 1
+    for i in stringdata:
+        if i == ',':
+            kolom += 1
+        if i == '\n':
+            break
+    return kolom
+
+# 4. Banyak baris pada csv
+def banyakbaris(namafile):
+    stringdata = bacacsv(namafile)
+    baris = 0
+    for i in stringdata:
+        if i == '\n':
+            baris += 1
+    return baris
+
+# 5. Ubah csv menjadi array yang bisa dimodifikasi
+def csvtoarray(namafile):
+    stringdata = bacacsv(namafile)
+    baris = banyakbaris(namafile)
+    kolom = banyakkolom(namafile)
+    chara = banyakchar(namafile)
+    # Inisiasi array data
+    arraydata = [["" for k in range (kolom)] for b in range (baris)]
+    start = 0
+    finish = -1
+    # mengisi arrydata dengan data dari csv
+    for b in range(baris):
+        for k in range(kolom):
+            start = finish + 1
+            if (k == kolom - 1):
+                finish = stringdata[start:chara].find('\n') + finish + 1
+            else:
+                finish = stringdata[start:chara].find(',') + finish + 1
+            arraydata[b][k] = stringdata[start:finish]
+    return arraydata
+
+
 # KUMPULAN SUBPROGRAM PENGOLAHAN DATA USER.CSV
-# 1. Mengubah user.csv menjadi array arraydatauser 
-datauser = open("user.csv")
-stringdata = datauser.read()
-datauser.close()
-# menambahkan "\n" jika belum
-if not(stringdata.endswith("\n")):
-    stringdata += "\n"
-
-# banyak char pada data
-chara = 0
-for row in stringdata:
-    chara += 1
-
-# definisi baris dan kolom (didefinisikan sebelumnya)  
-kolom = 6
-baris = 4
-
-# Inisiasi array data
-arraydatauser = [["" for k in range (kolom)] for b in range (baris)]
-start = 0
-finish = -1
-# mengisi arrydata dengan data dari csv
-for b in range(baris):
-    for k in range(kolom):
-        start = finish + 1
-        if (k == kolom - 1):
-            finish = stringdata[start:chara].find('\n') + finish + 1
-        else:
-            finish = stringdata[start:chara].find(',') + finish + 1
-        arraydatauser[b][k] = stringdata[start:finish]
+# 1. Inisialisasi
+arraydatauser = csvtoarray('user.csv')
+barisuser = banyakbaris('user.csv')
+kolomuser = banyakkolom('user.csv')
 
 # 2.
 def loginvalid(username,password):
     # Spesifikasi program : Memvalidasi apakah input username dan password sudah valid
 	# ALGORITMA
-    # definisi baris dan kolom (didefinisikan sebelumnya)  
-    kolom = 6
-    baris = 4
-
-    # Validasi username dan password
-    for b in range(baris):
+    for b in range(barisuser):
         if arraydatauser[b][1] == username and arraydatauser[b][3] == password:
             return True
 
@@ -56,12 +88,7 @@ def role(username,password):
     # Spesifikasi program : Menerima input username dan password serta mengeluarkan 
     # role yang dimiliki username
 	# ALGORITMA
-    # definisi baris dan kolom (didefinisikan sebelumnya)  
-    kolom = 6
-    baris = 4
-
-    # Cek role berdasar username
-    for b in range(baris):
+    for b in range(barisuser):
         if arraydatauser[b][1] == username and arraydatauser[b][3] == password:
             return arraydatauser[b][4]
 
@@ -69,12 +96,7 @@ def role(username,password):
 def nama(username):
     # Spesifikasi program : Menampilkan nama si pemilik username
 	# ALGORITMA
-    # definisi baris dan kolom (didefinisikan sebelumnya)  
-    kolom = 6
-    baris = 4
-
-    # outputkan nama berdasarkan username
-    for b in range(baris):
+    for b in range(barisuser):
         if arraydatauser[b][1] == username:
             return arraydatauser[b][2]
 
@@ -82,12 +104,7 @@ def nama(username):
 def registervalid(username):
     # Spesifikasi program : Memvalidasi apakah input username tidak pernah digunakan
 	# ALGORITMA
-    # definisi baris dan kolom (didefinisikan sebelumnya)  
-    kolom = 6
-    baris = 4
-
-    # Validasi username tidak pernah digunakan
-    for b in range(baris):
+    for b in range(barisuser):
         if arraydatauser[b][1] == username:
             return False
 
@@ -95,23 +112,13 @@ def registervalid(username):
 def nextnumber():
     # Spesifikasi program : menentukan penomoran untuk data berikutnya
 	# ALGORITMA
-    # definisi baris dan kolom (didefinisikan sebelumnya)  
-    kolom = 6
-    baris = 4
-
-    # Outputkan no data berikutnya
-    for b in range(baris-1,1,-1): # reverse loop
-        baris += 1
+    for b in range(barisuser-1,1,-1): # reverse loop
         return int(arraydatauser[b][0]) + 1
 
 # 7.
 def adduser(username_baru,nama,pswd_baru):
     # Spesifikasi program : Menambahkan data member baru pada list
     # ALGORITMA
-    # definisi baris dan kolom (didefinisikan sebelumnya)  
-    kolom = 6
-    baris = 4
-
     # Deklarasi array baru user
     baru = ['' for i in range(6)]
     # mengisi array baru dengan data user baru
