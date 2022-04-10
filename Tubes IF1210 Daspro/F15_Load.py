51e08effa15eb3a7ce8014e22226fed114d57a4a
rhmn5ty <92161339+rhmn5ty@users.noreply.github.com>
2022-04-10 11:30:03 +0700

    Add files via upload

---
+++ Tubes IF1210 Daspro/F15_Load.py
@@ -0,0 +1,39 @@
+import variables as var
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("nama_folder", help="loading semua file csv yang digunakan dalam program ini")
+args = parser.parse_args()
+
+cond = False
+for (path, directories, files) in os.walk(os.getcwd()):
+    for dir in directories:
+        if dir == args.nama_folder:
+            cond = True
+            print("Loading…")
+            print("# Panggil prosedur load data")
+            print("Selamat datang di antarmuka “Binomo”")
+            print("# Meminta perintah berikutnya… (cth: register, login, dll.)")
+            for (path2, directories2, files2) in os.walk(os.path.join(path, dir)):
+                for file in files2:
+                    if file == "game.csv":
+                        arr_game= var.pop_firstline(var.csvtoarray(os.path.join(path2, file)))
+                        baris_game= var.length(arr_game) 
+                        kolom_game = var.length(arr_game[0])
+                    if file == "kepemilikan.csv":
+                        arr_kepemilikan= var.pop_firstline(var.csvtoarray(os.path.join(path2, file)))
+                        baris_kepemilikan= var.length(arr_kepemilikan) 
+                        kolom_kepemilikan = var.length(arr_kepemilikan[0])
+                    if file == "riwayat.csv":
+                        arr_riwayat= var.pop_firstline(var.csvtoarray(os.path.join(path2, file)))
+                        baris_riwayat= var.length(arr_riwayat) 
+                        kolom_riwayat= var.length(arr_riwayat[0])
+                    if file == "user.csv":
+                        arr_user= var.pop_firstline(var.csvtoarray(os.path.join(path2, file)))
+                        baris_user= var.length(arr_user) 
+                        kolom_user = var.length(arr_user[0])
+                    
+if cond == False:
+    print('Folder "' + str(args.nama_folder) + '" tidak ditemukan.')
