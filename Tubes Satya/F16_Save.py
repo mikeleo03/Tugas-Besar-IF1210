fae94b64beed4380209c1647e235ceae15f9e46b
rhmn5ty <92161339+rhmn5ty@users.noreply.github.com>
2022-04-14 15:33:57 +0700

    Update F16_Save.py

--- Tubes Satya/F16_Save.py
+++ Tubes Satya/F16_Save.py
@@ -1,11 +1,28 @@
+# Program Save
+# Spesifikasi program : Program yang akan dijalankan jika pengguna ingin menyimpan data yang telah berubah
+
+# KAMUS
+# arrtocsv, save : procedure
+
+# ALGORITMA
+
+# import fungsi, prosedur, dan variabel buatan dari file lain
 import os
 import F15_Load as F15
 import variables as var
 
-
 def arrtocsv(array, nama_folder):
+    # Spesifikasi program : Mengubah array menjadi file csv yang tersimpan dalam suatu folder
+
+	# KAMUS LOKAL
+	# f : fungsi untuk memodifikasi suatu file
+    # array : array of array of string
+    # nama_folder : string
+    # i, j : integer
+
+	# ALGORITMA
     f = open(nama_folder, "w")
-    if nama_folder[-8:] == "user.csv":
+    if nama_folder[-8:] == "user.csv": # Mengubah array user menjadi file csv user
         f.write("id;username;nama;password;role;saldo\n")
         for i in range(var.length(array)):
             for j in range(6):
@@ -14,7 +31,7 @@ def arrtocsv(array, nama_folder):
                 else:
                     f.write(array[i][j] + "\n")
 
-    elif nama_folder[-8:] == "game.csv":
+    elif nama_folder[-8:] == "game.csv": # Mengubah array game menjadi file csv game
         f.write("id;nama;kategori;tahun_rilis;harga;stok\n")
         for i in range(var.length(array)):
             for j in range(6):
@@ -23,7 +40,7 @@ def arrtocsv(array, nama_folder):
                 else:
                     f.write(array[i][j] + "\n")
         
-    elif nama_folder[-11:] == "riwayat.csv":
+    elif nama_folder[-11:] == "riwayat.csv": # Mengubah array riwayat menjadi file csv riwayat
         f.write("game_id;nama;harga;user_id;tahun_beli\n")
         for i in range(var.length(array)):
             for j in range(5):
@@ -32,7 +49,7 @@ def arrtocsv(array, nama_folder):
                 else:
                     f.write(array[i][j] + "\n")
         
-    elif nama_folder[-15:] == "kepemilikan.csv":
+    elif nama_folder[-15:] == "kepemilikan.csv": # Mengubah array kepemilikan menjadi file csv kepemilikan
         f.write("game_id;user_id\n")
         for i in range(var.length(array)):
             for j in range(2):
@@ -43,6 +60,14 @@ def arrtocsv(array, nama_folder):
     f.close()
 
 def save(arr_game, arr_kepemilikan, arr_riwayat, arr_user):
+    # Spesifikasi program : Menyimpan file csv ke dalam suatu folder
+
+	# KAMUS LOKAL
+	# arr_game, arr_kepemilikan, arr_riwayat, arr_user : array of array of string
+    # nama_folder, lokasi : string
+    # arrtocsv : procedure
+
+	# ALGORITMA
     nama_folder = input("Masukkan nama folder penyimpanan: ")
     lokasi = F15.cari_folder(nama_folder)
     if lokasi == "":
